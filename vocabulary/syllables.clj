4346ed748e2bb3404486aa69bfb995c59fbd9e0b
Wojtek Franke <enajski@gmail.com>
2014-09-11 23:54:30 +0200

    Add docstrings for syllables

--- vocabulary/syllables.clj
+++ vocabulary/syllables.clj
@@ -2,13 +2,17 @@
   (:require [overtone.live :refer :all]))
 
 (def pu
+  "kick"
     (sample (freesound-path 26885)))
 
 (def ci
+  "hi-hat"
     (sample (freesound-path 26879)))
 
 (def ta
+  "snare"
     (sample (freesound-path 26900)))
 
 (def ty
+  "semi-snare"
     (sample (freesound-path 26896)))
