a9500af7776f0451606a3d6f9a913d339a8384c4
Wojtek Franke <enajski@gmail.com>
2016-09-28 16:41:54 +0200

    Bump Clojure and add leipzig

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "MIT"
             :url ""}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "LATEST"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha4"]
+                 [overtone "LATEST"]
+                 [leipzig "0.10.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
+)
