ad3f8f19e496ccca5dd6f9bf88f698667a06aea4
Wojtek Franke <enajski@gmail.com>
2016-09-28 16:42:25 +0200

    Fix indent

--- vocabulary/syllables.clj
+++ vocabulary/syllables.clj
@@ -3,16 +3,16 @@
 
 (def pu
   "kick"
-    (sample (freesound-path 26885)))
+  (sample (freesound-path 26885)))
 
 (def ci
   "hi-hat"
-    (sample (freesound-path 26879)))
+  (sample (freesound-path 26879)))
 
 (def ta
   "snare"
-    (sample (freesound-path 26900)))
+  (sample (freesound-path 26900)))
 
 (def ty
   "semi-snare"
-    (sample (freesound-path 26896)))
+  (sample (freesound-path 26896)))
